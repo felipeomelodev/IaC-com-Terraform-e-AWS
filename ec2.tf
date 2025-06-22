fa2b75e50e790f3e432f569ad962097ca1922a8a
Felipe Melo <felipe.o.melo@hotmail.com>
2025-06-22 16:10:19 -0300

    first commit

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web_server" {
+    ami           = data.aws_ami.amazon_linux.id
+    instance_type = "t2.micro"
+    user_data     = base64encode(file("user_data.sh"))
+
+    key_name      = aws_key_pair.ec2_key_pair.key_name
+
+    vpc_security_group_ids = [
+        aws_security_group.http_sg.id,
+        aws_security_group.ssh_sg.id,
+        aws_security_group.egress_all_sg.id
+    ]
+
+    tags = {
+        Name = "WebServer-DVP"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "meu_ip_publico" {
+    type        = string
+    description = "Endereço IP público para o Security Group SSH"
+    default     = "177.223.172.52/32"
+}
